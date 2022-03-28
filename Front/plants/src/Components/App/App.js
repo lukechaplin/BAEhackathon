9f02644d4a83c90e627fda9f01252f3ca0129675
Leanne251 <leannesmith251@gmail.com>
2022-03-28 14:22:05 +0100

    working on front end

--- Front/plants/src/Components/App/App.js
+++ Front/plants/src/Components/App/App.js
@@ -1,11 +1,26 @@
-import React from 'react';
+import { useState } from 'react';
+import SearchBar from '../SearchBar';
 
-function index() {
+function App() {
+	const [ search, setSearch ] = useState();
+	const [ data, setData ] = useState();
+
+	function getWord(e) {
+		setSearch(e.target.value);
+	}
+
+	async function submitInfo() {
+		const response = await fetch('');
+		const recievedData = await response.json();
+		setData(recievedData);
+	}
+
 	return (
 		<div>
 			<h1>Save My Plants!</h1>
+			<SearchBar submitInfo={submitInfo} getWord={getWord} />
 		</div>
 	);
 }
 
-export default index;
+export default App;
--- Front/plants/src/Components/SearchBar/index.js
+++ Front/plants/src/Components/SearchBar/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-function SearchBar({ getWord }) {
+function SearchBar({ getWord, submitInfo }) {
 	return (
 		<form onSubmit={submitInfo}>
 			<input type="text" placeholder="Search for a plant..." onChange={getWord} />
